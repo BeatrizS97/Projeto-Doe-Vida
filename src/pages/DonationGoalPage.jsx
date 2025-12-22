ec130e8f720d1994471441618c6bbb1811fdb78a
BeatrizS97 <bibia.wildestdreams@gmail.com>
2025-12-22 02:28:41 -0300

    refactor(DonationGoalPage): remover container redundante e simplificar layout
    - Remove divs de container e max-width do wrapper da página
    - Delega responsividade e espaçamento diretamente para o componente DonationGoal
    - Mantém o mesmo fundo e padding da página
    - Código mais limpo e consistente com o restante da aplicação

--- src/pages/DonationGoalPage.jsx
+++ src/pages/DonationGoalPage.jsx
@@ -5,14 +5,7 @@ import DonationGoal from '../components/DonationGoal';
 const DonationGoalPage = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-rose-50 via-white to-red-50 py-8 lg:py-12">
-      <div className="container mx-auto px-4 lg:px-6">
-        
-        {/* Componente de Meta - CONTÉM TODO O CONTEÚDO */}
-        <div className="max-w-4xl mx-auto">
-          <DonationGoal />
-        </div>
-
-      </div>
+      <DonationGoal />
     </div>
   );
 };
